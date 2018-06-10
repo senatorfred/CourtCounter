b851195f2b17d2b31ecb4ad7c928c94f3c458881
SenatorFred <opeyemifred@gmail.com>
2018-06-10 08:09:55 +0100

    fixed all errors

--- app/src/main/java/com/senatorfred/courtcounter/MainActivity.java
+++ app/src/main/java/com/senatorfred/courtcounter/MainActivity.java
@@ -7,17 +7,48 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void twoPoints(View view){
-        int twoPnts = 2;
+    int scores = 0;
+
+    public void twoPoints(View view) {
+        display(scores + 2);
 
     }
-}
+    public void threePoints(View view) {
+        display(scores + 3);
+
+    }
+    public void freethrow(View view) {
+        display(scores + 1);
+
+    }
+
+    public void twoPointsB(View view) {
+        displayB(scores + 2);
+
+    }
+    public void threePointsB(View view) {
+        displayB(scores + 3);
+
+    }
+    public void freethrowB(View view) {
+        displayB(scores + 1);
+
+    }
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.scoreTeamA);
-        quantityTextView.setText("" + number);
+        TextView opponentA = (TextView) findViewById(R.id.opponentA);
+        opponentA.setText(number);
+    }
+
+    private void displayB(int number){
+        TextView opponentB = (TextView)findViewById(R.id.opponentB);
+        opponentB.setText(number);
+    }
+
+}
