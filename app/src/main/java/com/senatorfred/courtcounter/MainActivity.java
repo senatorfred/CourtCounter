9b3423ebee0ce688cc215d40e726ea91672a7a8a
SenatorFred <opeyemifred@gmail.com>
2018-06-11 17:51:13 +0100

    fixed all errors

--- app/src/main/java/com/senatorfred/courtcounter/MainActivity.java
+++ app/src/main/java/com/senatorfred/courtcounter/MainActivity.java
@@ -8,47 +8,84 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
+    private TextView opponentA;
+    private TextView opponentB;
+    private int scores, scoresA = 0;
+    public int resetAll = 0;
+    public int resetAllB = 0;
+    private String resetAllNew = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_counter);
+        opponentA = (TextView) findViewById(R.id.opponentA);
+        opponentB = (TextView)findViewById(R.id.opponentB);
     }
 
-    int scores = 0;
+//     int scores = 0;
 
     public void twoPoints(View view) {
-        display(scores + 2);
+        scores = scores + 2;
+        display(scores);
+
 
     }
     public void threePoints(View view) {
-        display(scores + 3);
+        scores = scores + 3;
+        display(scores);
 
     }
     public void freethrow(View view) {
-        display(scores + 1);
+        scores  = scores + 1;
+        display(scores);
 
     }
 
     public void twoPointsB(View view) {
-        displayB(scores + 2);
+        scoresA = scoresA + 2;
+      displayB(scoresA);
 
     }
     public void threePointsB(View view) {
-        displayB(scores + 3);
+        scoresA = scoresA + 3;
+        displayB(scoresA);
 
     }
     public void freethrowB(View view) {
-        displayB(scores + 1);
+        scoresA = scoresA + 1;
+        displayB(scoresA);
+
+    }
+    public void resetAllScores(View view){
+        resetAll = 0;
+        scores = 0;
+
+    /*    resetAllNew = null;
+        resetAllNew = "0";
+
+        int _check = Integer.valueOf(resetAllNew);
+
+        */
+
+        display(resetAll);
+
+    }
+    public void resetAllScoresB(View view){
+        resetAllB = 0;
+        scoresA = 0;
+
+        displayB(resetAllB);
 
     }
     private void display(int number) {
-        TextView opponentA = (TextView) findViewById(R.id.opponentA);
-        opponentA.setText(number);
+
+        opponentA.setText(String.valueOf(number));
     }
 
     private void displayB(int number){
-        TextView opponentB = (TextView)findViewById(R.id.opponentB);
-        opponentB.setText(number);
+
+        opponentB.setText(String.valueOf(number));
     }
 
 }
